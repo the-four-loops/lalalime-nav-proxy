67befae8993a5c6a2edd1d6dacada99f066f2644
Michael Eskarous <mneskarous@gmail.com>
2019-09-06 19:13:50 -0700

    Able to render components without css or data

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// // import App1 from './components/App1';
+// // import App2 from './components/App2';
+// // import App3 from './components/App3';
+// // import App4 from './components/App4';
+
+// ReactDOM.render(
+//   <div></div>,
+//   document.getElementById("app"));
---
+++ server/app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const port = 3000;
+
+const navbar = proxy({
+  target: 'http://localhost:3001'
+});
+
+const mainProduct = proxy({
+  target: 'http://localhost:3002'
+});
+
+const similarProducts = proxy({
+  target: 'http://localhost:3003'
+});
+
+const reviews = proxy({
+  target: 'http://localhost:3004'
+});
+
+// const options = {
+//   target: 'http://localhost:3000', 
+//   router: {
+//     'navbar' : 'http://localhost:3001',
+//     'main-product' : 'http://localhost:3002',
+//     'similar-products' : 'http://localhost:3003',
+//     'reviews' : 'http://localhost:3004'
+//   }
+// };
+
+const app = express();
+// const apiProxy = proxy(options);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+// app.use(
+//   '/',
+//   proxy({ target: 'http://localhost:3000/' })
+// );
+// app.use('/', apiProxy); 
+app.use('/navbar', navbar);
+app.use('/main-product', mainProduct);
+app.use('/similar-products', similarProducts);
+app.use('/reviews', reviews);
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`app is listening on port ${port}`));
